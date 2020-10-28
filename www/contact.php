e1ad37d4dc250f5b57cfd5d8ba1d8ecda16173eb
Jackson Carvalho de Melo <jacksonmelo7740@gmail.com>
2020-10-28 02:11:37 -0300

    Confirmação inicial2

--- www/contact.php
+++ www/contact.php
@@ -94,7 +94,7 @@
 
             <div class="row sticky-parent">
                 <!-- Sidebar nav -->
-                <aside class="col-12 col-md-12 col-lg-2">
+                < class="col-12 col-md-12 col-lg-2">
                     <div class="sidebar box sticky-column">
                         <ul class="nav">
                             <li class="nav__item"><a class="active" href="index.html"><i class="icon-user"></i>Sobre
@@ -106,71 +106,71 @@
                             <li class="nav__item"><a href="contact.html"><i class="icon-book"></i>Contato</a></li>
                         </ul>
                     </div>
-                </aside>
+                    </aside>
 
-                <!-- Conteudo -->
-                <div class="col-12 col-md-12 col-lg-10">
-                    <div class="box box-content">
-                        <div class="pb-2">
-                            <h1 class="title title--h1 first-title title__separate">Contato</h1>
+                    <!-- Conteudo -->
+                    <div class="col-12 col-md-12 col-lg-10">
+                        <div class="box box-content">
+                            <div class="pb-2">
+                                <h1 class="title title--h1 first-title title__separate">Contato</h1>
+                            </div>
+
+                            <!-- Contato -->
+
+                            <form action="email.php" method="post" id="contact-form" class="contact-form"
+                                data-toggle="validator">
+                                <div class="row">
+                                    <div class="form-group col-12 col-md-6">
+                                        <input type="text" class="form-control" id="nameContact" name="nameContact"
+                                            placeholder="Nome" required="required" autocomplete="on"
+                                            oninvalid="setCustomValidity('Fill in the field')"
+                                            onkeyup="setCustomValidity('')">
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                    <div class="form-group col-12 col-md-6">
+                                        <input type="phone" class="form-control" id="phoneContact" name="phoneContact"
+                                            placeholder="Telefone" required="required" autocomplete="on"
+                                            oninvalid="setCustomValidity('Email is incorrect')"
+                                            onkeyup="setCustomValidity('')">
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                    <div class="form-group col-12 col-md-12">
+                                        <input type="email" class="form-control" id="emailContact" name="emailContact"
+                                            placeholder="Email" required="required" autocomplete="on"
+                                            oninvalid="setCustomValidity('Email is incorrect')"
+                                            onkeyup="setCustomValidity('')">
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                    <div class="form-group col-12 col-md-12">
+                                        <textarea class="textarea form-control" id="messageContact"
+                                            name="messageContact" placeholder="Mensagem" rows="4" required="required"
+                                            oninvalid="setCustomValidity('Fill in the field')"
+                                            onkeyup="setCustomValidity('')"></textarea>
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-12 col-md-6 order-2 order-md-1 text-center text-md-left">
+                                        <div id="validator-contact" class="hidden"></div>
+                                    </div>
+                                    <div class="col-12 col-md-6 order-1 order-md-2 text-right">
+                                        <button type="submit" class="btn"><i class="font-icon icon-send"></i>
+                                            Enviar</button>
+                                    </div>
+                                </div>
+                            </form><br><br><br>
+
+                            <div class="map" id="map">
+                                <iframe
+                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3654.824943304019!2d-46.82046378548918!3d-23.646439470623644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce54a94ba3ac0d%3A0x86dcf699b8be9412!2sParque%20Luiza%2C%20Embu%20das%20Artes%20-%20SP%2C%2006810-240!5e0!3m2!1spt-BR!2sbr!4v1599504382367!5m2!1spt-BR!2sbr"
+                                    width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""
+                                    aria-hidden="false" tabindex="0"></iframe>
+                            </div>
                         </div>
 
-                        <!-- Contato -->
-
-                        <form action="email.php" method="post" id="contact-form" class="contact-form"
-                            data-toggle="validator">
-                            <div class="row">
-                                <div class="form-group col-12 col-md-6">
-                                    <input type="text" class="form-control" id="nameContact" name="nameContact"
-                                        placeholder="Nome" required="required" autocomplete="on"
-                                        oninvalid="setCustomValidity('Fill in the field')"
-                                        onkeyup="setCustomValidity('')">
-                                    <div class="help-block with-errors"></div>
-                                </div>
-                                <div class="form-group col-12 col-md-6">
-                                    <input type="phone" class="form-control" id="phoneContact" name="phoneContact"
-                                        placeholder="Telefone" required="required" autocomplete="on"
-                                        oninvalid="setCustomValidity('Email is incorrect')"
-                                        onkeyup="setCustomValidity('')">
-                                    <div class="help-block with-errors"></div>
-                                </div>
-                                <div class="form-group col-12 col-md-12">
-                                    <input type="email" class="form-control" id="emailContact" name="emailContact"
-                                        placeholder="Email" required="required" autocomplete="on"
-                                        oninvalid="setCustomValidity('Email is incorrect')"
-                                        onkeyup="setCustomValidity('')">
-                                    <div class="help-block with-errors"></div>
-                                </div>
-                                <div class="form-group col-12 col-md-12">
-                                    <textarea class="textarea form-control" id="messageContact" name="messageContact"
-                                        placeholder="Mensagem" rows="4" required="required"
-                                        oninvalid="setCustomValidity('Fill in the field')"
-                                        onkeyup="setCustomValidity('')"></textarea>
-                                    <div class="help-block with-errors"></div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-12 col-md-6 order-2 order-md-1 text-center text-md-left">
-                                    <div id="validator-contact" class="hidden"></div>
-                                </div>
-                                <div class="col-12 col-md-6 order-1 order-md-2 text-right">
-                                    <button type="submit" class="btn"><i class="font-icon icon-send"></i>
-                                        Enviar</button>
-                                </div>
-                            </div>
-                        </form><br><br><br>
-
-                        <div class="map" id="map">
-                            <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3654.824943304019!2d-46.82046378548918!3d-23.646439470623644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce54a94ba3ac0d%3A0x86dcf699b8be9412!2sParque%20Luiza%2C%20Embu%20das%20Artes%20-%20SP%2C%2006810-240!5e0!3m2!1spt-BR!2sbr!4v1599504382367!5m2!1spt-BR!2sbr"
-                                width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""
-                                aria-hidden="false" tabindex="0"></iframe>
-                        </div>
+                        <!-- Footer -->
+                        <footer class="footer">© Jackson Carvalho de Melo</footer>
                     </div>
-
-                    <!-- Footer -->
-                    <footer class="footer">© Jackson Carvalho de Melo</footer>
-                </div>
             </div>
         </div>
     </main>
